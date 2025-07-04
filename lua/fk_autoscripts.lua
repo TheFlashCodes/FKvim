4180173e24827e37dc5076c6438d24a08e103937
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-07-04 14:53:52 +0530

    Autoscript to run FKui in Fkvim Environment and Neotree  v1.1.1 LST

--- lua/fk_autoscripts.lua
+++ lua/fk_autoscripts.lua
@@ -1,3 +1,18 @@
+-- Initializing FKui as default Buffer for FKvim
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- Check if Neovim was opened with a folder
+    if vim.fn.argc() == 0 and vim.fn.isdirectory(vim.fn.getcwd()) == 1 then
+      -- Close the default buffer
+      vim.cmd("enew")         -- New empty buffer
+      vim.cmd("Dashboard")    -- Open FKvim Dashboard
+    end
+  end,
+})
+
+-- AutoLoading File Explorer
+
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     if vim.fn.argc() == 0 and vim.fn.isdirectory(vim.fn.getcwd()) == 1 then
