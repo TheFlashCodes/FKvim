13b0a97f05576d8b7822811812701493e58db7b2
Mayank Kumar Jha <138496086+flashcodes-themayankjha@users.noreply.github.com>
2025-08-08 17:54:09 +0530

    True Transparency Support

--- lua/fk_plugins/fkui.lua
+++ lua/fk_plugins/fkui.lua
@@ -2,6 +2,22 @@
 
 vim.cmd.colorscheme("catppuccin")
 
+-- Set transparent background for specific UI components
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "WhichKey", { bg = "none" })
+vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "LazyNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "LazyFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "CmpPmenu", { bg = "none" })
+vim.api.nvim_set_hl(0, "CmpPmenuSel", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "none" })
+vim.api.nvim_set_hl(0, "PmenuSbar", { bg = "none" })
+vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "none" })
+
 -- Turn off cursorline in normal mode, enable it in insert mode
 vim.api.nvim_create_autocmd("InsertEnter", {
   pattern = "*",
@@ -16,4 +32,3 @@ vim.api.nvim_create_autocmd("InsertLeave", {
     vim.wo.cursorline = false
   end,
 })
-
